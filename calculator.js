8667fca9164c76b320e6f2d77b85ba5c3da0b33a
FlynnAuto <107214627+FlynnAuto@users.noreply.github.com>
2023-11-02 00:19:31 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,16 +1,14 @@
 document.getElementById("calculateButton").addEventListener("click", function() {
     const loanAmount = parseFloat(document.getElementById("loanAmount").value);
-    const annualInterestRate = parseFloat(document.getElementById("interestRate").value) / 100;
-    const monthlyInterestRate = annualInterestRate / 12;
+    const interestRate = parseFloat(document.getElementById("interestRate").value) / 100;
     const loanTerm = parseFloat(document.getElementById("loanTerm").value);
 
-    if (loanAmount && annualInterestRate && loanTerm) {
-        const numberOfPayments = loanTerm;
-        const monthlyPayment = loanAmount * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
-        
-        document.getElementById("monthlyPayment").textContent = "$" + monthlyPayment.toFixed(2);
-    } else {
-        // Handle invalid or missing inputs
-        document.getElementById("monthlyPayment").textContent = "Invalid inputs";
-    }
+    const monthlyInterestRate = interestRate / 12;
+    const numberOfPayments = loanTerm;
+    
+    const monthlyPayment = (loanAmount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
+    const totalPayment = monthlyPayment * numberOfPayments;
+
+    document.getElementById("monthlyPayment").textContent = "$" + monthlyPayment.toFixed(2);
+    document.getElementById("totalPayment").textContent = "$" + totalPayment.toFixed(2);
 });
