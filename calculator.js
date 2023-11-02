5f775d1ed516d447df9c2bc17e740d3e067ec66f
FlynnAuto <107214627+FlynnAuto@users.noreply.github.com>
2023-11-02 00:37:43 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,11 +1,15 @@
 document.getElementById("calculateButton").addEventListener("click", function() {
     const vehiclePrice = parseFloat(document.getElementById("vehiclePrice").value);
+    const tradeInAmount = parseFloat(document.getElementById("tradeInAmount").value);
     const annualInterestRate = parseFloat(document.getElementById("interestRate").value) / 100;
     const monthlyInterestRate = annualInterestRate / 12;
     const loanTerm = parseFloat(document.getElementById("loanTerm").value);
 
     if (vehiclePrice && annualInterestRate && loanTerm) {
-        const principalAmount = vehiclePrice;
+        // Calculate the principal amount as (vehicle price - trade-in) + 13% taxes
+        const taxRate = 0.13;
+        const principalAmount = (vehiclePrice - tradeInAmount) * (1 + taxRate);
+
         const numberOfPayments = loanTerm;
         const monthlyPayment = principalAmount * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
 
