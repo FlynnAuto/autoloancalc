e8769ddb018bd986866503147fa8be7dff181e71
FlynnAuto <107214627+FlynnAuto@users.noreply.github.com>
2023-11-02 00:15:56 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,16 @@
+document.getElementById("calculateButton").addEventListener("click", function() {
+    const loanAmount = parseFloat(document.getElementById("loanAmount").value);
+    const annualInterestRate = parseFloat(document.getElementById("interestRate").value) / 100;
+    const monthlyInterestRate = annualInterestRate / 12;
+    const loanTerm = parseFloat(document.getElementById("loanTerm").value);
 
+    if (loanAmount && annualInterestRate && loanTerm) {
+        const numberOfPayments = loanTerm;
+        const monthlyPayment = loanAmount * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+        
+        document.getElementById("monthlyPayment").textContent = "$" + monthlyPayment.toFixed(2);
+    } else {
+        // Handle invalid or missing inputs
+        document.getElementById("monthlyPayment").textContent = "Invalid inputs";
+    }
+});
